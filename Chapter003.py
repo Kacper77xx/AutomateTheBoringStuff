59bf4c94d01e27646f17e378e3f3033f6ce0f48c
Kacper77xx <kacper77xx@gmail.com>
2023-05-07 23:12:39 +0200

    First Commit

--- Chapter003.py
+++ Chapter003.py
@@ -12,25 +12,35 @@
 # hello('Peter')
 # hello(input())
 
-import random
-def getAnswer(answerNumber):
-    if answerNumber == 1:
-        return 'Of course'
-    elif answerNumber == 2:
-        return 'Sure'
-    elif answerNumber == 3:
-        return 'Yes'
-    elif answerNumber == 4:
-        return 'Not sure'
-    elif answerNumber == 5:
-        return 'Ask me later'
-    elif answerNumber == 6:
-        return 'Think about it again and answer'
-    elif answerNumber == 7:
-        return 'My answer is no'
-    elif answerNumber == 8:
-        return 'It looks not good'
-    elif answerNumber == 9:
-        return 'Very bad'
-r = random.randint(1, 9)
-print(getAnswer(r))
+# import random
+# def getAnswer(answerNumber):
+#     if answerNumber == 1:
+#         return 'Of course'
+#     elif answerNumber == 2:
+#         return 'Sure'
+#     elif answerNumber == 3:
+#         return 'Yes'
+#     elif answerNumber == 4:
+#         return 'Not sure'
+#     elif answerNumber == 5:
+#         return 'Ask me later'
+#     elif answerNumber == 6:
+#         return 'Think about it again and answer'
+#     elif answerNumber == 7:
+#         return 'My answer is no'
+#     elif answerNumber == 8:
+#         return 'It looks not good'
+#     elif answerNumber == 9:
+#         return 'Very bad'
+# print(getAnswer(random.randint(1, 9)))
+
+# spam = print('Hello!')
+# print(None == spam)
+
+# print('Hello, ', end = '')
+# print('world!')
+#
+# print('cats', 'dogs', 'mouses')
+# print('cats', 'dogs', 'mouses', sep = ', ')
+# print('cats', 'dogs', 'mouses', sep = ',')
+
