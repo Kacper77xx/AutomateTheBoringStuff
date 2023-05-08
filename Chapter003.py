d1b91277ec3ff13efdaa1b54c421dfdd0f3d66a1
Kacper77xx <kacper77xx@gmail.com>
2023-05-08 17:29:14 +0200

    First Commit

--- Chapter003.py
+++ Chapter003.py
@@ -78,3 +78,13 @@
 # spam()
 # print(eggs)
 
+# def spam():
+#     global eggs
+#     eggs = 'spam'
+# def bacon():
+#     eggs = 'bacon'
+# def ham():
+#     print(eggs)
+# eggs = 42
+# spam()
+# print(eggs)
