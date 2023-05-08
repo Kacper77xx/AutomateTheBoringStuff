1bf6f102cb69848ef53fece8e998484d176e9f6e
Kacper77xx <kacper77xx@gmail.com>
2023-05-08 17:14:02 +0200

    First Commit

--- Chapter003.py
+++ Chapter003.py
@@ -44,3 +44,37 @@
 # print('cats', 'dogs', 'mouses', sep = ', ')
 # print('cats', 'dogs', 'mouses', sep = ',')
 
+# def spam():
+#     eggs = 99
+#     bacon()
+#     print(eggs)
+# def bacon():
+#     ham = 101
+#     eggs = 0
+# spam()
+
+# def spam():
+#     print(eggs)
+# eggs = 42
+# spam()
+# print(eggs)
+
+# def spam():
+#     eggs = 'Local scope spam()'
+#     print(eggs)
+# def bacon():
+#     eggs = 'Local scope bacon()'
+#     print(eggs)
+#     spam()
+#     print(eggs)
+# eggs = 'Global scope'
+# bacon()
+# print(eggs)
+
+# def spam():
+#     global eggs
+#     eggs = 'spam'
+# eggs = 'Global scope'
+# spam()
+# print(eggs)
+
