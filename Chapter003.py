903b8ff265c563bc3eab415d5268baafa5183be8
Kacper77xx <kacper77xx@gmail.com>
2023-05-15 15:20:10 +0200

    First Commit

--- Chapter003.py
+++ Chapter003.py
@@ -115,14 +115,18 @@
 # else:
 #     print('Unlucky, maybe next time!')
 
+print('Give me a number!')
+try:
+    number = int(input())
+except  ValueError:
+    print('Enter a valid integer')
 def collatz(number):
     if number % 2 == 0:
         print(number // 2)
         return number//2
     elif number % 2 == 1:
-        result = 3 * number + 1
-        print(result)
-        return result
+        print(3 * number + 1)
+        return 3 * number + 1
 number = input("Give me a number: ")
 while number != 1:
     number = collatz(int(number))
